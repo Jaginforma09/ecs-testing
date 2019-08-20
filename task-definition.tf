82d5227e504212527e34756f670eafef55b9ee20
mavrick202 <mavrick202@gmail.com>
2019-08-20 12:23:16 +0530

    ecs-baseline

---
+++ task-definition.tf
@@ -0,0 +1,24 @@
+data "aws_ecs_task_definition" "nginx" {
+  task_definition = "${aws_ecs_task_definition.nginx.family}"
+}
+
+resource "aws_ecs_task_definition" "nginx" {
+    family                = "hello_world"
+    container_definitions = <<DEFINITION
+[
+  {
+    "name": "nginx",
+     "image": "721834156908.dkr.ecr.us-east-1.amazonaws.com/demo:latest",
+    "essential": true,
+    "portMappings": [
+      {
+        "containerPort": 80,
+        "hostPort": 80
+      }
+    ],
+    "memory": 500,
+    "cpu": 10
+  }
+]
+DEFINITION
+}
